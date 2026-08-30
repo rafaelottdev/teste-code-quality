0f21a484176c60ab29280d63d360161fca9768d7
Rafael Ott <157972006+rafaelottdev@users.noreply.github.com>
2026-08-30 18:07:07 -0300

    fix lint erros

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ const user = {
   email: 'email@email.com',
 };
 
-const users = [{ name: 'Rafael', age: 25 },
+const users = [
+  { name: 'Rafael', age: 25 },
   { name: 'João', age: 30 },
   { name: 'Maria', age: 28 },
 ];
 
-const handleUserClick = (user: { name: string; age: number }, event: MouseEvent) => {
-  console.log(user, event);
+const handleUserClick = (user: { name: string; age: number, email: string }, users: {name:string, age:number}[]) => {
+  console.log(user, users);
 };
+
+handleUserClick(user, users)
